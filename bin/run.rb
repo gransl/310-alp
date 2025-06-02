303f4dc7081ab4fea75cb99f6dc43d716c157e5e
Sandra <gran.sl@gmail.com>
2025-06-01 17:59:42 -0700

    fix main

--- bin/run.rb
+++ bin/run.rb
@@ -13,22 +13,28 @@ cg = data.create_cell_group
 # # Question 1
 puts 'Max Average Body Weight by company: '
 print cg.max_avg_body_weight_by_company
+puts ''
+puts ''
 
 # # Question 2
 puts 'List of phones released in a year different from their announcement:'
 print cg.announce_yr_different_than_release_yr
+puts ''
+puts ''
 
 # # Question 3
 puts 'Count of phones with only one feature sensor:'
 print cg.one_feature_phone_count
+puts ''
+puts ''
 
 # # Question 4
 puts 'Phone Launch count per year: '
 x = cg.phone_count_per_year
-print x
 x.each do |year, count|
   puts "#{year} : #{count}"
 end
+puts ''
 
 # Other functions
 puts "Avg. body weight (g): #{cg.avg_body_weight}"
@@ -36,10 +42,11 @@ puts "Standard deviation, body weight: #{cg.sd_body_weight}"
 puts ''
 puts "Avg. display size (in): #{cg.avg_display_size}"
 puts "Standard devation, display size: #{cg.sd_display_size}"
+puts''
 
 year = 2018
 puts "Phones announced in #{year}:"
-puts ''
+
 puts cg.phones_announced_in_year(year)
 
 puts ''
